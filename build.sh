c32e7b5570b41f1d4177c1053fcdbd3acdb05dbe
TisuHitam <94881976+TisuHitam@users.noreply.github.com>
2022-02-25 16:19:05 +0700

    Delete build.sh

--- build.sh
+++
@@ -1,5 +0,0 @@
-# If you already install g++ or build-essential, use this
-sudo apt-get install clang
-sudo apt-get install boxes
-clang++ script/CPPBot.cpp script/enet/callbacks.c script/enet/compress.c script/enet/host.c script/enet/list.c script/enet/packet.c script/enet/peer.c script/enet/protocol.c script/enet/unix.c -pthread -std=c++11 -fpermissive -o cid -w #Build source code into executable
-echo "Build Successfully! you just do ./cid on terminal"
